4b04353e089b2d105094532a21800685bc8b87a9
Ramón Lozano <niksokupa@gmail.com>
2018-12-24 02:07:26 +0100

    meh

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: any ) {
+this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
